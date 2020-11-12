4e9b486a4166723bd47eb4271243441f7edc82f4
gabrieliusmm <55552995+gabrieliusmm@users.noreply.github.com>
2020-11-12 10:23:37 +0200

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,57 @@
+let show = document.querySelector('button.rodyti');
+let mix = document.querySelector('button.maisyti');
+let save = document.querySelector('button.saugoti');
+
+let nuotraukudiv = document.querySelector('div.nuotraukos');
+
+show.addEventListener('click',showimg);
+mix.addEventListener('click',miximages);
+save.addEventListener('click',saveimages)
+
+let images = ['logo1.png','logo2.png','logo3.png','logo4.png','logo5.png'];
+const path = "img/";
+function showimg() {
+	let imagelist = document.querySelector('.nuotraukos ul')
+	for(let i = 0; i < images.length; i++) {
+		let li = document.createElement('li');
+		imagelist.appendChild(li);
+		let imgElement = document.createElement('img');
+		li.appendChild(imgElement);
+		imgElement.src=path+images[i];
+		show.style.display="none";
+	}
+	let imgElementas = document.querySelectorAll('.nuotraukos ul li img');
+	for(let p = 0; p<imgElementas.length; p++) {
+		imgElementas[p].ondblclick = function(e) {
+			console.log('pressed');
+			e.target.src = path + 'gif/clap.gif';
+		}
+	}
+}
+function miximages() {
+	images.sort(function() {return 0.5 - Math.random()});
+	let imgElementai = document.querySelectorAll('.nuotraukos img');
+	for(i = 0; i< images.length; i++) {
+		imgElementai[i].src = path + images[i];
+	}
+}
+function saveimages() {
+	let imgElementai = document.querySelectorAll('.nuotraukos img');
+	for(i = 0; i< images.length; i++) {
+		localStorage.setItem("image"+i, imgElementai[i].src);
+		console.log(imgElementai[i])
+	}
+}
+let print = document.querySelector('button.print')
+print.addEventListener('click',printout);
+function printout() {
+	let imagelist = document.querySelector('.savedimg ul')
+	for(let i = 0; i < images.length; i++) {
+		let li = document.createElement('li');
+		imagelist.appendChild(li);
+		let imgElement = document.createElement('img');
+		li.appendChild(imgElement);
+		imgElement.src=localStorage.getItem('image'+i);
+		print.style.display="none";
+	}
+}
